82d43ae9d9b1231dfb084706d4a527e53539b193
VincentB00 <vincentbui1999@gmail.com>
2023-07-10 18:02:26 -0400

    init

---
+++ src/test/java/com/vincent/inc/File_manager/FileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.File_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
